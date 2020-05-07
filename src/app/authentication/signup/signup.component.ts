f537d6bd843dc137c3b38fe8d3bcaec817b6c2f7
Yuvi R <yuvaranjani.rajendran@gmail.com>
2020-05-06 22:55:07 -0500

    1. Datepicker min and max validation 2. Added Material Checkbox 3. Button validation - disabling
    **** Completed Signup Form ******

--- src/app/authentication/signup/signup.component.ts
+++ src/app/authentication/signup/signup.component.ts
@@ -8,10 +8,18 @@ import { NgForm } from '@angular/forms';
 })
 
 export class SignupComponent implements OnInit {
+  minAge;
+  maxAge;
 
   constructor() { }
 
   ngOnInit(): void {
+    this.minAge = new Date(); /* This will return the current date */
+    this.maxAge = new Date();
+    this.maxAge.setFullYear(this.maxAge.getFullYear() - 18); /* This will subtract 18 years from Current date */
+    this.minAge.setFullYear(this.minAge.getFullYear() - 50); /* This will subtract 50 years from Current date */
+    console.log("Max Age: " + this.maxAge);
+    console.log("Min Age: " + this.minAge);
   }
 
   submitSignUpForm(objSignUpForm: NgForm) {
--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -8,6 +8,7 @@ import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { FlexModule } from '@angular/flex-layout';
 import { MatDatepickerModule } from '@angular/material/datepicker';
+import {MatCheckboxModule} from '@angular/material/checkbox';
 
 @NgModule({
   declarations: [],
@@ -20,7 +21,8 @@ import { MatDatepickerModule } from '@angular/material/datepicker';
     MatFormFieldModule,
     FlexModule,
     MatNativeDateModule,
-    MatDatepickerModule
+    MatDatepickerModule,
+    MatCheckboxModule
   ],
   exports: [
     BrowserAnimationsModule, 
@@ -30,7 +32,8 @@ import { MatDatepickerModule } from '@angular/material/datepicker';
     MatFormFieldModule,
     FlexModule,
     MatNativeDateModule,
-    MatDatepickerModule
+    MatDatepickerModule,
+    MatCheckboxModule
   ]
 })
 export class MaterialModule { } 
